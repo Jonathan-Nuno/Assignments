44805070fa3ca5e12dd484df0bd3fe6a79e4873d
Jonathan Nuno <jnuno.423@gmail.com>
2020-09-10 18:35:21 -0400

    added second solution

--- d2-a3.py
+++ d2-a3.py
@@ -1,16 +1,38 @@
 ### Day 2 - Assignment 3 Prime Number detector
 
 ##Solution 1
-print("Prime number detector")
+#print("Prime number detector")
 
-while True:
-    pn = int(input("Please enter a number: "))
-    for index in range(2, pn):
-        if pn % index == 0:
-            print("is not a prime number!")
-            break
-    else:
-        print(pn, "is a prime number!")
-        break
+#while True:
+#    pn = int(input("Please enter a number: "))
+#    for index in range(2, pn):
+#        if pn % index == 0:
+#            print("is not a prime number!")
+#            break
+#    else:
+#        print(pn, "is a prime number!")
+#        break
 
 ### WORK IN PROGRESSfunction with T/F statement to return if prime or not
+print("Prime number detector")
+
+def prim(n):
+    if n < 2:
+        return False
+    if n == 2: 
+        return True
+    if not n & 1:
+        return False
+    for index in range(3, pn, 2):
+        if n % index == 0:
+            return False
+
+    return True
+
+pn = int(input("Please enter your number: "))
+ans = prim(pn)
+
+if (ans):
+    print("It is a prime number")
+else:
+    print("It is not a prime number")
