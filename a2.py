8eece59d2111809c2b4f082fdd9dcb889c4ffbb6
Jonathan Nuno <jnuno.423@gmail.com>
2020-09-10 14:18:56 -0400

    removed old files

--- a2.py
+++
@@ -1,8 +0,0 @@
-### Assignment 2 - Even Odd
-
-print("Is your number even or odd?")
-num = int(input("Please enter your number: "))
-if (num % 2) == 0:
-    print(f"{num} is Even")
-else:
-    print(f"{num} is Odd")
