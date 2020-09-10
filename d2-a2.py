159273e65fa37e26c12307a913e5d29854c92722
Jonathan Nuno <jnuno.423@gmail.com>
2020-09-10 17:45:10 -0400

    added second solution

--- d2-a2.py
+++ d2-a2.py
@@ -1,14 +1,29 @@
 ### Day 2 - Assignment 2 - Palindrome detector
 
+###Solution 1
+#print("Palindrome Checker")
+
+#stc = input("Enter a word: ")
+#if stc == stc [::-1]:
+#    print("This word is a palindrome")
+#else:
+#    print("This word is not a palindrome")
+
+
+### Solution 2
+
 print("Palindrome Checker")
 
-###Solution 1
-stc = input("Enter a word: ")
-if stc == stc [::-1]:
-    print("This word is a palindrome")
+def palin(n):
+    for index in range(0,int(len(n)/2)):
+        if n[index] != n[len(n) -index -1]:
+            return False
+        return True
+
+w = input("Please enter a word: ")
+ans = palin(w)
+
+if (ans):
+    print("Yes")
 else:
-    print("This word is not a palindrome")
-
-
-#### WORK IN PROGRESS for loop to find palindrome
-#### WORK IN PROGRESS while loop to find palindrome
+    print("No")
