e5a17e3e6ec6db316f0c522c186269f9060ed6f3
Jonathan Nuno <jnuno.423@gmail.com>
2020-10-08 17:32:43 -0400

    added option to add individual grocery items to specified stores

--- week_5/js/d4-grocery.js
+++ week_5/js/d4-grocery.js
@@ -11,16 +11,29 @@ addGroceryStore.addEventListener('click', () => {
     saveToFirebase(name, address)
 })
 
+
 // function to add items to firebase
 function saveToFirebase(name, address) {
     db.collection('stores').add({
         name: name,
         address: address,
+        items: []
     }).then(function (docRef) {
         allStores()
     }).catch(function (error) {
         console.log(error)
     })
+}
+
+function addGroceryItems(storeId) {
+    storeNameIdTextBox = document.getElementById(storeId)
+    sName = storeNameIdTextBox.value
+    db.collection('stores').doc(storeId)
+        .update({
+            items: firebase.firestore.FieldValue.arrayUnion(` ${sName}`)
+        }).then(function() {
+            allStores()
+        })
 }
 
 function deleteStore(documentId) {
@@ -41,12 +54,23 @@ function allStores() {
             let data = doc.data()
             let storeInfo = `<li>
             <label>${data.name} - ${data.address}</label>
-            <button onclick="deleteStore('${doc.id}')">Remove</button>
+            <input type="text" id=${doc.id} placeholder="Enter Grocery Item" />
+            <button onclick="addGroceryItems('${doc.id}')">Add Item</button>
+            <p> - ${nullChecker(data.items)}</p>
+            <button onclick="deleteStore('${doc.id}')">Remove List</button>
             </li>`
 
             storeList.insertAdjacentHTML('beforeend', storeInfo)
         })
     })
+}
+
+function nullChecker(item) {
+    if(item == "") {
+        return `<b>Grocery list is empty</b>`
+    } else {
+        return item
+    }
 }
 
 allStores()
