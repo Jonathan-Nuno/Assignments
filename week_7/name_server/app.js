713871bc67940f73602bc0b345a654c52ef9059b
Jonathan Nuno <jnuno.423@gmail.com>
2020-10-19 11:56:04 -0400

    learned and added paramaters to assignment local database

--- week_7/name_server/app.js
+++ week_7/name_server/app.js
@@ -6,6 +6,20 @@ app.get('/name', (req, res) => {
     res.json(names)
 })
 
+app.get('/name/:category', (req, res) => {
+    let category = req.params.category
+    console.log(req.params.category)
+    
+    res.send('Name' + category)
+})
+
 app.listen(3000, () => {
     console.log('Server is running...')
+})
+
+app.get('/digital-crafts/cohort/:year', (req, res) => {
+    let year = req.params.year
+    console.log(req.params.year)
+
+    res. send('I study at DigitalCrafts ' + year + ' Cohort')
 })
